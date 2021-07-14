546a0cc54e3fcf5e01c55cd0f1477f9c3cb14e32
Алена Микитюк <alenamikituk@Alena.local>
2021-07-14 20:15:48 +0300

    initial commit

---
+++ src/menu.js
@@ -0,0 +1,34 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  themeSwitchToggle: document.getElementById('theme-switch-toggle'),
+  body: document.querySelector('body'),
+  themeSwitchToggle: document.querySelector('.theme-switch__toggle'),
+};
+
+refs.themeSwitchToggle.addEventListener('change', onThemeSwitchToggle);
+window.addEventListener('load', onPageLoad, { once: true });
+
+function onThemeSwitchToggle(e) {
+  if (e.currentTarget.checked === true) {
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.body.classList.remove(Theme.DARK);
+    refs.body.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function onPageLoad() {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    refs.body.classList.add(Theme.DARK);
+    refs.themeSwitchToggle.checked = true;
+  } else {
+    refs.body.classList.add(Theme.LIGHT);
+  }
+}
